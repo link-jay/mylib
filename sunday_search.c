742efc2ca4cc3b6a4093ec4e5bdc099987601f54
li huijie <li.huijie.lhj@qq.com>
2026-04-28 19:02:00 +0800

    修改sunday_search使其兼容hashtable的更改

--- sunday_search.c
+++ sunday_search.c
@@ -2,20 +2,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "HashTable.c"
+#include "hashtable.c"
 
 int sundaySearch(char* main_str, char* sub_str) {
-  
-  HashTable* shift_table = create_ht(32);
-  int main_len    = strlen(main_str);
-  int sub_len     = strlen(sub_str);
+  typedef struct _Node {struct _Node *next; int value; char key[];} _Node;
+  struct HashTable {size_t len; size_t cap; _Node** buckets;} shift_table = {};
+  int main_len = strlen(main_str);
+  int sub_len = strlen(sub_str);
   int current_pos = 0;
-  int max_pos     = main_len - sub_len;
+  int max_pos = main_len - sub_len;
   
   for (int relative_pos = 0; relative_pos < sub_len; relative_pos++) {
-    char key[]   = {sub_str[relative_pos], '\0'};
+    char key[] = {sub_str[relative_pos], '\0'};
     int offset = sub_len - relative_pos;
-    ht_put(&shift_table, key, offset);
+    HT_PUT(shift_table.buckets, shift_table.len, shift_table.cap, key, offset);
   }
   
   while (current_pos <= max_pos) {
@@ -27,31 +27,42 @@ int sundaySearch(char* main_str, char* sub_str) {
     }
     
     if (relative_pos == sub_len) {
-      del_ht(shift_table);
+      FREE_HT(shift_table.buckets, shift_table.cap);
       return current_pos;
     }
     
     char next_char[] = {main_str[current_pos + sub_len], '\0'};
-    int k;
-    if ((k = ht_get(shift_table, next_char)) != -1) {
+    int k = -1;
+    HT_GET(shift_table.buckets, shift_table.len, shift_table.cap, next_char, k);
+    if (k != -1) {
       current_pos += k;
     } else {
       current_pos += sub_len;
     }
   }
-  del_ht(shift_table);
+  FREE_HT(shift_table.buckets, shift_table.cap);
   return -1;
 }
 
 int main()
 {
-  FILE* fp = fopen("test.txt", "r");
+  /* FILE* fp = fopen("test.txt", "r"); */
   
-  char *main_str, *sub_str;
-  size_t n1 = 0;
-  size_t n2 = 0;
-  getline(&main_str, &n1, fp); main_str[strchr(main_str, '\n') - main_str] = '\0';
-  getline(&sub_str, &n2, fp); sub_str[strchr(sub_str, '\n') - sub_str] = '\0';
+  /* char *main_str = NULL; */
+  /* char *sub_str = NULL; */
+  /* size_t n1 = 0; */
+  /* size_t n2 = 0; */
+  
+  /* getline(&main_str, &n1, fp); */
+  /* char *newline1 = strchr(main_str, '\n'); */
+  /* if (newline1) *newline1 = '\0'; */
+
+  /* getline(&sub_str, &n2, fp); */
+  /* char *newline2 = strchr(sub_str, '\n'); */
+  /* if (newline2) *newline2 = '\0'; */
+  
+  char main_str[] = "hello, world";
+  char sub_str[] = "world";
   
   int pos = sundaySearch(main_str, sub_str);
   printf("pos: %d\n", pos);
